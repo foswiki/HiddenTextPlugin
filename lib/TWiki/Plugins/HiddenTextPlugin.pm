f7e4ea8c56e7a39b57e47872a9fcf5c0ca14418d
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/HiddenTextPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/HiddenTextPlugin.pm
+++ lib/TWiki/Plugins/HiddenTextPlugin.pm
@@ -11,7 +11,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -37,24 +37,24 @@
 #   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
 #   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
 #
-# initPlugin is required, all other are optional. 
+# initPlugin is required, all other are optional.
 # For increased performance, all handlers except initPlugin are
 # disabled. To enable a handler remove the leading DISABLE_ from
 # the function name. Remove disabled handlers you do not need.
 #
-# NOTE: To interact with TWiki use the official TWiki functions 
+# NOTE: To interact with TWiki use the official TWiki functions
 # in the TWiki::Func module. Do not reference any functions or
 # variables elsewhere in TWiki!!
 
-
 # =========================
-package TWiki::Plugins::HiddenTextPlugin;    # change the package name and $pluginName!!!
+package TWiki::Plugins::HiddenTextPlugin
+  ;    # change the package name and $pluginName!!!
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-        $debug $exampleCfgVar
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $pluginName
+  $debug $exampleCfgVar
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -66,36 +66,39 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-$pluginName = 'HiddenTextPlugin';  # Name of this Plugin
+$pluginName = 'HiddenTextPlugin';    # Name of this Plugin
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
+    $debug = TWiki::Func::getPreferencesFlag("\U$pluginName\E_DEBUG");
 
-    # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
-    $exampleCfgVar = &TWiki::Func::getPreferencesValue( "EMPTYPLUGIN_EXAMPLE" ) || "default";
+ # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
+    $exampleCfgVar = &TWiki::Func::getPreferencesValue("EMPTYPLUGIN_EXAMPLE")
+      || "default";
 
     # Plugin correctly initialized
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+    TWiki::Func::writeDebug(
+        "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
+      if $debug;
     return 1;
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::commonTagsHandler( $_[2].$_[1] )")
+      if $debug;
 
     # This is the place to define customized tags and variables
     # Called by sub handleCommonTags, after %INCLUDE:"..."%
@@ -104,58 +107,60 @@ sub commonTagsHandler
     # $_[0] =~ s/%XYZ%/&handleXyz()/ge;
     # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/ge;
 
-	$_[0] =~ s/%STARTHIDDEN%(.*?)%ENDHIDDEN%/$1/sg;
+    $_[0] =~ s/%STARTHIDDEN%(.*?)%ENDHIDDEN%/$1/sg;
 }
 
 # =========================
-sub beforeEditHandler
-{
+sub beforeEditHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::beforeEditHandler( $_[2].$_[1] )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::beforeEditHandler( $_[2].$_[1] )")
+      if $debug;
 
-    # This handler is called by the edit script just before presenting the edit text
-    # in the edit box. Use it to process the text before editing.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
+# This handler is called by the edit script just before presenting the edit text
+# in the edit box. Use it to process the text before editing.
+# New hook in TWiki::Plugins $VERSION = '1.010'
 
-	if ($_[0] =~ /%STARTHIDDEN%(.*?)%ENDHIDDEN%/s)
-	{
-		my $wikiUser = TWiki::Func::getWikiUserName();	
-		if (!TWiki::Func::checkAccessPermission("hidden", $wikiUser, $_[0], $_[1], $_[2]))
-		{
-			$_[0] =~ s/(%STARTHIDDEN%(.*?)%ENDHIDDEN%)//sg;
-			my $storage = $1;
-			$storage =~ s/&amp\;/&/go;    
-			$storage =~ s/&lt\;/</go;
-			$storage =~ s/&gt\;/>/go;
-			my $filename = "/tmp/tmp${_[2]}${_[1]}";
-			open TMPFILE, ">$filename";
-			print TMPFILE $storage;
-			close TMPFILE;
-		}
-	}
+    if ( $_[0] =~ /%STARTHIDDEN%(.*?)%ENDHIDDEN%/s ) {
+        my $wikiUser = TWiki::Func::getWikiUserName();
+        if (
+            !TWiki::Func::checkAccessPermission(
+                "hidden", $wikiUser, $_[0], $_[1], $_[2]
+            )
+          )
+        {
+            $_[0] =~ s/(%STARTHIDDEN%(.*?)%ENDHIDDEN%)//sg;
+            my $storage = $1;
+            $storage =~ s/&amp\;/&/go;
+            $storage =~ s/&lt\;/</go;
+            $storage =~ s/&gt\;/>/go;
+            my $filename = "/tmp/tmp${_[2]}${_[1]}";
+            open TMPFILE, ">$filename";
+            print TMPFILE $storage;
+            close TMPFILE;
+        }
+    }
 }
 
 # =========================
-sub afterEditHandler
-{
+sub afterEditHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::afterEditHandler( $_[2].$_[1] )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::afterEditHandler( $_[2].$_[1] )")
+      if $debug;
 
-    # This handler is called by the preview script just before presenting the text.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
+ # This handler is called by the preview script just before presenting the text.
+ # New hook in TWiki::Plugins $VERSION = '1.010'
 
-	my $filename = "/tmp/tmp${_[2]}${_[1]}";
-	if ( -e $filename )
-	{
-		undef $/;	# read file all at once
-		open TMPFILE, "$filename";
-		my $storage = <TMPFILE>;
-		close TMPFILE;
-		unlink $filename;
-		$_[0] = "$storage $_[0]";
-	}
+    my $filename = "/tmp/tmp${_[2]}${_[1]}";
+    if ( -e $filename ) {
+        undef $/;    # read file all at once
+        open TMPFILE, "$filename";
+        my $storage = <TMPFILE>;
+        close TMPFILE;
+        unlink $filename;
+        $_[0] = "$storage $_[0]";
+    }
 }
 
 1;
